57654a5b7dd5e60d3310099ee079a177b8a0c374
Martin Eduardo Mebs <martin.mebs@sensustec.com.br>
2025-04-22 19:26:37 -0300

    Criando modal para cadastro de tarefas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TarefasPageComponent } from './pages/tarefas-page/tarefas-page.component';
+import { MaterialModule } from './shared/modules/material/material.module';
+import { MenuComponent } from './shared/components/menu/menu.component';
+import { ModalTarefaComponent } from './components/modal-tarefa/modal-tarefa.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TarefasPageComponent,
+    MenuComponent,
+    ModalTarefaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/tarefas-page/tarefas-page.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalTarefaComponent } from '../../components/modal-tarefa/modal-tarefa.component';
+
+@Component({
+  selector: 'app-tarefas-page',
+  standalone: false,
+  templateUrl: './tarefas-page.component.html',
+  styleUrl: './tarefas-page.component.css'
+})
+export class TarefasPageComponent {
+
+  constructor(public matDialog: MatDialog) {}
+
+  abrirModalCadastroTarefa() {
+    const tarefaDialog = this.matDialog.open(ModalTarefaComponent, {
+      width: "500px",
+      height: "300px"
+    });
+
+  }
+}
